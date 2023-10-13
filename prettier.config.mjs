847bcf1a0f780c6ac9c55d1c3acc8949f5cc931f
Sean Nicholas Dieterle <dieterle@sodefa.de>
2023-10-13 14:00:27 +0200

    🎉 t3 app

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+}
+
+export default config
