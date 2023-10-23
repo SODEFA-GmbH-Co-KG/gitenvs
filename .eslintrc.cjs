cef3a7a948db4c5e502a7102f23f5706d8ab8a0e
Sean Nicholas Dieterle <dieterle@sodefa.de>
2023-10-23 22:57:04 +0200

    💚

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ const config = {
     // Feel free to reconfigure them to your own preference.
     '@typescript-eslint/array-type': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
 
     '@typescript-eslint/consistent-type-imports': [
       'warn',
