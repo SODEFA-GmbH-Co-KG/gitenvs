4c9f95a07a4b97bf9e1f7fe4d513aabf0ff07274
Sean Nicholas Dieterle <dieterle@sodefa.de>
2023-10-25 14:09:08 +0200

    💄

--- @/components/SetupGitenvs.tsx
+++ @/components/SetupGitenvs.tsx
@@ -50,68 +50,90 @@ export const SetupGitenvs = () => {
   })
 
   return (
-    <div>
-      <h1 className="text-2xl mb-12 text-center">Setup Gitenvs</h1>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit((data) => {
-            console.log(data)
-          })}
-        >
-          <div className="flex flex-row gap-4">
-            <div className="flex flex-col gap-4 flex-1">
-              <h2 className="text-lg">Env File</h2>
-              <div className="flex flex-row gap-4">
-                <FormField
-                  control={form.control}
-                  name="envFile.name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="envFile.type"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Type</FormLabel>
-                        <Select
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                        >
-                          <FormControl>
-                            <SelectTrigger className="w-[180px]">
-                              <SelectValue placeholder="Type" />
-                            </SelectTrigger>
-                          </FormControl>
-                          <SelectContent>
-                            {EnvFileType.options.map((option) => (
-                              <SelectItem key={option} value={option}>
-                                {option}
-                              </SelectItem>
-                            ))}
-                          </SelectContent>
-                        </Select>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit((data) => {
+          console.log(data)
+        })}
+        className="flex flex-col gap-8"
+      >
+        <h1 className="text-2xl text-center">Setup Gitenvs</h1>
+        <div className="flex flex-row gap-4">
+          <div className="flex flex-col gap-4 flex-1">
+            <h2 className="text-lg">Env File</h2>
+            <div className="flex flex-row gap-4">
+              <FormField
+                control={form.control}
+                name="envFile.name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <div>
                 <FormField
                   control={form.control}
-                  name="envFile.filePath"
+                  name="envFile.type"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>File Path</FormLabel>
+                      <FormLabel>Type</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Type" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {EnvFileType.options.map((option) => (
+                            <SelectItem key={option} value={option}>
+                              {option}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="envFile.filePath"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>File Path</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+          <div className="w-[1px] bg-gray-200"></div>
+
+          <div className="flex flex-col gap-4 flex-1">
+            <h2 className="text-lg">Stages</h2>
+            <FormLabel>Name</FormLabel>
+            {envStages.fields.map((field, index) => (
+              <div className="flex flex-row gap-4" key={field.id}>
+                <FormField
+                  control={form.control}
+                  name={`envStages.${index}.name`}
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
                       <FormControl>
                         <Input {...field} />
                       </FormControl>
@@ -119,49 +141,30 @@ export const SetupGitenvs = () => {
                     </FormItem>
                   )}
                 />
+                <Button
+                  type="button"
+                  variant="outline"
+                  onClick={() => envStages.remove(index)}
+                  disabled={envStages.fields.length === 1}
+                >
+                  🗑️
+                </Button>
               </div>
-            </div>
-
-            <div className="w-[1px] bg-gray-200"></div>
-
-            <div className="flex flex-col gap-4 flex-1">
-              <h2 className="text-lg">Stages</h2>
-              <FormLabel>Name</FormLabel>
-              {envStages.fields.map((field, index) => (
-                <div className="flex flex-row gap-4" key={field.id}>
-                  <FormField
-                    control={form.control}
-                    name={`envStages.${index}.name`}
-                    render={({ field }) => (
-                      <FormItem className="flex-1">
-                        <FormControl>
-                          <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button
-                    type="button"
-                    variant="outline"
-                    onClick={() => envStages.remove(index)}
-                    disabled={envStages.fields.length === 1}
-                  >
-                    🗑️
-                  </Button>
-                </div>
-              ))}
-              <Button
-                type="button"
-                onClick={() => envStages.append({ name: '' })}
-                variant="outline"
-              >
-                Add Stage
-              </Button>
-            </div>
+            ))}
+            <Button
+              type="button"
+              onClick={() => envStages.append({ name: '' })}
+              variant="outline"
+            >
+              Add Stage
+            </Button>
           </div>
-        </form>
-      </Form>
-    </div>
+        </div>
+
+        <Button type="submit" variant="default">
+          Let's go
+        </Button>
+      </form>
+    </Form>
   )
 }
