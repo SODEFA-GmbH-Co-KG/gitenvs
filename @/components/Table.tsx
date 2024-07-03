a56af457b6dd4a3fa6dec8967a28d9249f10ad89
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-03 17:59:41 +0200

    ⚰️

--- @/components/Table.tsx
+++ @/components/Table.tsx
@@ -3,38 +3,16 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import NiceModal from '@ebay/nice-modal-react'
-import { map } from 'lodash-es'
 import { Fragment, useEffect } from 'react'
-import { useFieldArray } from 'react-hook-form'
-import { encryptEnvVar } from '~/gitenvs/encryptEnvVar'
-import { Gitenvs } from '~/gitenvs/gitenvs.schema'
 import { getNewEnvVarId } from '~/gitenvs/idsGenerator'
 import { api } from '~/utils/api'
-import { useZodForm } from '~/utils/useZodForm'
 import { EditDialog } from './EditDialog'
 
 export const Table = ({ fileId }: { fileId: string }) => {
   const trpcUtils = api.useUtils()
-  const { data: gitenvs } = api.gitenvs.getGitenvs.useQuery(undefined, {
-    // TODO: This is not how you should do it. onSuccess will be deprecated in future.
-    onSuccess: (data) => {
-      if (form.formState.isDirty) return
-      form.reset(data)
-    },
-  })
+  const { data: gitenvs } = api.gitenvs.getGitenvs.useQuery()
   const { mutateAsync: saveGitenvs } = api.gitenvs.saveGitenvs.useMutation()
 
-  const form = useZodForm({
-    schema: Gitenvs,
-    // defaultValues: gitenvs,
-  })
-
-  const envVarsFields = useFieldArray({
-    control: form.control,
-    name: 'envVars',
-  })
-
-  const envVars = envVarsFields.fields
   const columns = (gitenvs?.envStages.length ?? 0) + 1
 
   useEffect(() => {
@@ -95,64 +73,8 @@ export const Table = ({ fileId }: { fileId: string }) => {
   }, [columns])
 
   return (
-    <form
-      // TODO: Handle zod parsing errors
-      onSubmit={form.handleSubmit(async (newGitenvs) => {
-        const encryptedEnvVars = await Promise.all(
-          newGitenvs.envVars.map(async (envVar) => {
-            const oldEnvVar = gitenvs?.envVars.find(
-              (envVar) =>
-                envVar.fileId === envVar.fileId && envVar.key === envVar.key,
-            )
-
-            const valuesEntries = await Promise.all(
-              map(Object.entries(envVar.values), async (data) => {
-                const [stageName, value] = data
-
-                if (!value.encrypted) return data
-
-                if (value.value === oldEnvVar?.values[stageName]?.value) {
-                  return data
-                }
-
-                const publicKey = gitenvs?.envStages.find(
-                  (stage) => stage.name === stageName,
-                )?.publicKey
-
-                if (!publicKey) throw new Error('No public key found')
-
-                return [
-                  stageName,
-                  {
-                    ...value,
-                    value: await encryptEnvVar({
-                      plaintext: value.value,
-                      publicKey,
-                    }),
-                  },
-                ] as const
-              }),
-            )
-
-            const values = Object.fromEntries(valuesEntries)
-
-            return {
-              ...envVar,
-              values,
-            }
-          }),
-        )
-        await saveGitenvs({
-          gitenvs: {
-            ...newGitenvs,
-            envVars: encryptedEnvVars,
-          },
-        })
-      })}
-      className="flex flex-col gap-2"
-      autoComplete="new-password"
-    >
-      {!!envVars.length ? (
+    <div className="flex flex-col gap-2">
+      {!!gitenvs?.envVars.length ? (
         <div
           className="grid gap-2"
           id="supergrid"
@@ -253,6 +175,6 @@ export const Table = ({ fileId }: { fileId: string }) => {
       <Button variant="outline" type="submit">
         Save
       </Button>
-    </form>
+    </div>
   )
 }
