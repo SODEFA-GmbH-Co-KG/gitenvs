0e024369ebf49bf15c4fdb5bb4a60d269ac6badd
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-04 17:01:23 +0200

    ♻️ shortcuts

--- @/components/EnvFileSwitcher.tsx
+++ @/components/EnvFileSwitcher.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import { Plus } from 'lucide-react'
+import { redirect } from 'next/navigation'
 import { Fragment } from 'react'
 import { type Gitenvs } from '~/gitenvs/gitenvs.schema'
 import {
@@ -17,55 +18,33 @@ export const EnvFileSwitcher = ({
   gitenvs: Gitenvs
   activeFileId: string
 }) => {
-  // const router = useRouter()
-
-  // const openNewEnvFileDialog = async () => {
-  //   await NiceModal.show(NewEnvFileDialog, { gitenvs })
-  // }
-
-  // useEffect(() => {
-  //   const handler = async (event: KeyboardEvent) => {
-  //     if (document.activeElement instanceof HTMLInputElement) {
-  //       return
-  //     }
-
-  //     event.preventDefault()
-
-  //     if (event.key.toLowerCase() === 'n') {
-  //       await openNewEnvFileDialog()
-  //       return
-  //     }
-
-  //     let key = parseInt(event.key, 10)
-  //     if (Number.isNaN(key)) return
-  //     if (key === 0) key = 10
-  //     if (key >= 0 && key <= Math.min(10, gitenvs.envFiles.length)) {
-  //       const id = gitenvs.envFiles[key - 1]?.id
-  //       if (!id) return
-  //       router.push(`/file/${id}`)
-  //     }
-  //   }
-  //   document.addEventListener('keydown', handler)
-  //   return () => {
-  //     document.removeEventListener('keydown', handler)
-  //   }
-  // }, [gitenvs, openNewEnvFileDialog, router])
-
   return (
     <div className="flex flex-row gap-4">
       {gitenvs.envFiles.map((envFile, index) => {
         return (
           <Fragment key={envFile.id}>
-            <a
-              href={`/file/${envFile.id}`}
+            <ActionButton
+              variant="vanilla"
+              size={'vanilla'}
+              hideIcon
+              action={async () => {
+                'use server'
+                return superAction(async () => {
+                  redirect(`/file/${envFile.id}`)
+                })
+              }}
               className={cn(
                 'flex h-7 flex-row items-center justify-center gap-2 rounded-full px-4 text-center text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-primary',
                 envFile.id === activeFileId && 'bg-muted text-primary',
               )}
+              command={{
+                shortcut: { key: `${index + 1}` },
+                label: `Switch to ${envFile.name}`,
+              }}
             >
               <span>{envFile.name}</span>
               <KeyShortcut>{index + 1}</KeyShortcut>
-            </a>
+            </ActionButton>
           </Fragment>
         )
       })}
@@ -77,6 +56,7 @@ export const EnvFileSwitcher = ({
         className={cn(
           'flex h-7 flex-row items-center justify-center gap-2 rounded-full px-4 text-center text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-primary',
         )}
+        command={{ shortcut: { key: 'N' }, label: 'New Env File' }}
         action={async () => {
           'use server'
           return superAction(async () => {
