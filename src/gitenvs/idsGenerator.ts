0546582d021c599db3c6bc9a1751ec28f5cfdbea
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-04 13:38:15 +0200

    ✨ use nanoid for fileId

--- src/gitenvs/idsGenerator.ts
+++ src/gitenvs/idsGenerator.ts
@@ -1,5 +1,9 @@
 import { nanoid } from 'nanoid'
 
+export const getNewEnvFileId = () => {
+  return `envFile_${nanoid()}`
+}
+
 export const getNewEnvVarId = () => {
   return `envVar_${nanoid()}`
 }
