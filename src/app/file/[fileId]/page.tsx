b135a12ee2a5c8ba2218eaa58abd8a90bd25231e
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-05 14:06:53 +0200

    ✨ redirect if file does not exist

--- src/app/file/[fileId]/page.tsx
+++ src/app/file/[fileId]/page.tsx
@@ -1,9 +1,14 @@
 import { EnvFileSwitcher } from '@/components/EnvFileSwitcher'
 import { Table } from '@/components/Table'
+import { redirect } from 'next/navigation'
 import { getGitenvs } from '~/gitenvs/gitenvs'
 
 export default async function Page({ params }: { params: { fileId: string } }) {
   const gitenvs = await getGitenvs()
+
+  if (!gitenvs.envFiles.find((file) => file.id === params.fileId)) {
+    redirect(`/`)
+  }
 
   return (
     <div className="flex flex-col gap-4">
