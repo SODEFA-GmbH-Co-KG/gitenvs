2d95a9493b19d4e62ffdef1a4b0eacdc97126b22
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-05 10:16:07 +0200

    ✨ make type selectable

--- @/components/NewEnvFileDialog.tsx
+++ @/components/NewEnvFileDialog.tsx
@@ -2,10 +2,9 @@ import { Button } from '@/components/ui/button'
 import { DialogFooter } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { revalidatePath } from 'next/cache'
-import { z } from 'zod'
 import { saveGitenvs } from '~/gitenvs/gitenvs'
-import { type Gitenvs } from '~/gitenvs/gitenvs.schema'
-import { getNewEnvVarId } from '~/gitenvs/idsGenerator'
+import { EnvFile, EnvFileType, type Gitenvs } from '~/gitenvs/gitenvs.schema'
+import { getNewEnvFileId } from '~/gitenvs/idsGenerator'
 import {
   streamDialog,
   streamToast,
@@ -13,41 +12,15 @@ import {
 } from '~/super-action/action/createSuperAction'
 import { ActionForm } from '~/super-action/form/ActionForm'
 import { Label } from './ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
 
 export const NewEnvFileDialog = ({ gitenvs }: { gitenvs: Gitenvs }) => {
-  // const { mutateAsync: saveGitenvs } = api.gitenvs.saveGitenvs.useMutation()
-  // const utils = api.useUtils()
-  // const modal = useModal()
-  // const [name, setName] = useState('')
-  // const [filePath, setFilePath] = useState('')
-
-  // const done = () => {
-  //   modal.resolve()
-  //   modal.remove()
-  // }
-
-  // const newEnvFile = async () => {
-  //   await saveGitenvs({
-  //     gitenvs: {
-  //       ...gitenvs,
-  //       envFiles: [
-  //         ...gitenvs.envFiles,
-  //         {
-  //           id: getNewEnvVarId(),
-  //           name,
-  //           filePath,
-  //           // TODO: Set type
-  //           type: 'dotenv',
-  //         },
-  //       ],
-  //     },
-  //   })
-
-  //   await utils.gitenvs.invalidate()
-
-  //   done()
-  // }
-
   return (
     <div className="sm:max-w-[425px]">
       <ActionForm
@@ -55,21 +28,16 @@ export const NewEnvFileDialog = ({ gitenvs }: { gitenvs: Gitenvs }) => {
           'use server'
 
           return superAction(async () => {
-            const name = z.string().parse(formData?.get('name'))
-            const filePath = z.string().parse(formData?.get('filePath'))
+            const newEnvFile = EnvFile.parse({
+              id: getNewEnvFileId(),
+              name: formData?.get('name'),
+              filePath: formData?.get('filePath'),
+              type: formData?.get('type'),
+            })
 
             await saveGitenvs({
               ...gitenvs,
-              envFiles: [
-                ...gitenvs.envFiles,
-                {
-                  id: getNewEnvVarId(),
-                  name,
-                  filePath,
-                  // TODO: Set type
-                  type: 'dotenv',
-                },
-              ],
+              envFiles: [...gitenvs.envFiles, newEnvFile],
             })
 
             streamDialog(null)
@@ -84,11 +52,31 @@ export const NewEnvFileDialog = ({ gitenvs }: { gitenvs: Gitenvs }) => {
         className="flex flex-col gap-4"
       >
         <div className="flex flex-col gap-4">
-          <Label htmlFor="name">Name</Label>
-          <Input type="text" autoComplete="off" name="name" />
           <Label htmlFor="filePath">File Path</Label>
           <Input type="text" autoComplete="off" name="filePath" />
         </div>
+        <div className="flex flex-row justify-stretch gap-4">
+          <div className="flex flex-1 flex-col gap-4">
+            <Label htmlFor="name">Name</Label>
+            <Input type="text" autoComplete="off" name="name" />
+          </div>
+          <div className="flex flex-1 flex-col gap-4">
+            <Label htmlFor="type">Type</Label>
+            <Select name="type">
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                {EnvFileType.options.map((option) => (
+                  <SelectItem key={option} value={option}>
+                    {option}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
         <DialogFooter>
           <Button type="submit">Save</Button>
         </DialogFooter>
