6a130fdccc15bb20b65ebbae6a20b1ee6e666d29
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-07-09 15:35:20 +0200

    ✨ readd add button

--- @/components/Table.tsx
+++ @/components/Table.tsx
@@ -1,6 +1,11 @@
 import { Input } from '@/components/ui/input'
+import { map } from 'lodash-es'
 import { Fragment } from 'react'
+import { saveGitenvs } from '~/gitenvs/gitenvs'
 import { type Gitenvs } from '~/gitenvs/gitenvs.schema'
+import { getNewEnvVarId } from '~/gitenvs/idsGenerator'
+import { superAction } from '~/super-action/action/createSuperAction'
+import { ActionButton } from '~/super-action/button/ActionButton'
 import { EnvKeyMenu } from './EnvKeyMenu'
 import { TableEnvKey } from './TableEnvKey'
 import { TableEnvVar } from './TableEnvVar'
@@ -83,28 +88,36 @@ export const Table = ({
         ) : (
           <p>No env vars so far. Add a new one</p>
         )}
-        {/* <Button
-          type="button"
-          onClick={async () => {
-            if (!gitenvs) return
-            const values = Object.fromEntries(
-              gitenvs.envStages.map((stage) => [
-                stage.name,
-                { value: '', encrypted: true },
-              ]),
-            )
-            const newGitenvs = {
-              ...gitenvs,
-              envVars: [
-                ...gitenvs.envVars,
-                { id: getNewEnvVarId(), fileId, key: '', values },
-              ],
-            }
-            await saveGitenvs(newGitenvs)
+        <ActionButton
+          command={{
+            shortcut: {
+              key: 'a',
+            },
+            label: 'Add new env var',
+          }}
+          action={async () => {
+            'use server'
+
+            return superAction(async () => {
+              const values = Object.fromEntries(
+                map(gitenvs.envStages, (stage) => [
+                  stage.name,
+                  { value: '', encrypted: true },
+                ]),
+              )
+              const newGitenvs = {
+                ...gitenvs,
+                envVars: [
+                  ...gitenvs.envVars,
+                  { id: getNewEnvVarId(), fileId, key: '', values },
+                ],
+              }
+              await saveGitenvs(newGitenvs)
+            })
           }}
         >
           Add
-        </Button> */}
+        </ActionButton>
       </div>
     </Fragment>
   )
