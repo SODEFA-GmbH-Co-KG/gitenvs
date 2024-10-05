969df145e58da073195beea2ce6c84233a79c6da
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-05 08:47:41 +0200

    ⚰️ remove trpc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Toaster as ToasterSonner } from 'sonner'
 import '~/styles/globals.css'
 import { ActionCommandProvider } from '~/super-action/command/ActionCommandProvider'
 import { DialogProvider } from '~/super-action/dialog/DialogProvider'
-import { TrpcProvider } from '~/utils/TrpcProvider'
 import { Providers } from './providers'
 
 export default async function RootLayout({
@@ -14,19 +13,17 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <TrpcProvider>
-          <Providers>
-            <main className="flex min-h-screen flex-col items-center">
-              <div className="container flex flex-col items-center gap-12 px-4 pb-16 pt-40">
-                {children}
-              </div>
-            </main>
-            <ActionCommandProvider />
-            <DialogProvider />
-            <Toaster />
-            <ToasterSonner />
-          </Providers>
-        </TrpcProvider>
+        <Providers>
+          <main className="flex min-h-screen flex-col items-center">
+            <div className="container flex flex-col items-center gap-12 px-4 pb-16 pt-40">
+              {children}
+            </div>
+          </main>
+          <ActionCommandProvider />
+          <DialogProvider />
+          <Toaster />
+          <ToasterSonner />
+        </Providers>
       </body>
     </html>
   )
