b1e8a7fe2ef187d43b16aa2730f61f02fec84c9d
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-14 22:22:05 +0200

    ✨ new action button

--- @/components/KeyShortcut.tsx
+++ @/components/KeyShortcut.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react'
 
 export const KeyShortcut = ({ children }: { children?: ReactNode }) => {
   return (
-    <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted p-1 font-mono text-[8px] font-medium text-muted-foreground opacity-100">
+    <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted p-1 font-mono text-[9px] font-medium text-muted-foreground opacity-100">
       {children}
     </kbd>
   )
--- src/super-action/button/ActionButton.tsx
+++ src/super-action/button/ActionButton.tsx
@@ -4,21 +4,13 @@ import { getCmdCtrlKey, KeyShortcut } from '@/components/KeyShortcut'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { ArrowRight, Loader2 } from 'lucide-react'
-import {
-  forwardRef,
-  type ComponentPropsWithoutRef,
-  type ReactNode,
-} from 'react'
-import {
-  useSuperAction,
-  type UseSuperActionOptions,
-} from '../action/useSuperAction'
-import { ActionCommand } from '../command/ActionCommand'
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react'
+import { UseSuperActionOptions } from '../action/useSuperAction'
 import { type ActionCommandConfig } from '../command/ActionCommandProvider'
+import { ActionWrapper, ActionWrapperSlotProps } from './ActionWrapper'
 
-export type ActionButtonProps<Comp extends typeof Button = typeof Button> = {
+export type ActionButtonProps = {
   children?: React.ReactNode
-  component?: Comp
   hideIcon?: boolean
   hideButton?: boolean
   command?: Omit<
@@ -29,19 +21,13 @@ export type ActionButtonProps<Comp extends typeof Button = typeof Button> = {
     hideKeyShortcut?: boolean
   }
 } & UseSuperActionOptions &
-  ComponentPropsWithoutRef<Comp>
+  ComponentPropsWithoutRef<typeof Button>
 
-export const ActionButton = forwardRef(
-  <Comp extends typeof Button = typeof Button>(
-    props: ActionButtonProps<Comp>,
-    ref: React.ForwardedRef<HTMLButtonElement>,
-  ) => {
+export const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(
+  (props, ref) => {
     const {
       action,
       disabled,
-      children,
-      component: Component = Button,
-      hideIcon = true,
       hideButton,
       catchToast = true,
       askForConfirmation,
@@ -49,59 +35,55 @@ export const ActionButton = forwardRef(
       command,
       ...buttonProps
     } = props
-    const { isLoading, trigger } = useSuperAction({
-      action,
-      disabled,
-      catchToast,
-      askForConfirmation,
-      stopPropagation,
-    })
-    const Icon = isLoading ? Loader2 : ArrowRight
 
     return (
       <>
-        {!hideButton && (
-          <Component
-            ref={ref}
-            type="button"
-            disabled={isLoading ?? disabled}
-            {...buttonProps}
-            onClick={trigger}
-          >
-            <div className="flex flex-row items-center gap-2">
-              {children}
-              {command?.hideKeyShortcut ?? !command?.shortcut?.key ? null : (
-                <div className="flex flex-row gap-1">
-                  {command?.shortcut?.shift && <KeyShortcut>Shift</KeyShortcut>}
-                  {command?.shortcut?.cmdCtrl && (
-                    <KeyShortcut>{getCmdCtrlKey()}</KeyShortcut>
-                  )}
-                  <KeyShortcut>
-                    {command?.shortcut?.key.toUpperCase()}
-                  </KeyShortcut>
-                </div>
-              )}
-            </div>
-            {!hideIcon && (
-              <Icon
-                className={cn('ml-2 h-4 w-4', isLoading && 'animate-spin')}
-              />
-            )}
-          </Component>
-        )}
-        {command && (
-          <ActionCommand
-            icon={hideIcon ? undefined : Icon}
-            {...command}
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            action={trigger as any} // TODO: fix type
-          >
-            {command.label ?? children}
-          </ActionCommand>
-        )}
+        <ActionWrapper
+          action={action}
+          disabled={disabled}
+          askForConfirmation={askForConfirmation}
+          stopPropagation={stopPropagation}
+          command={command}
+          catchToast={catchToast}
+          triggerOn={['onClick']}
+        >
+          {!hideButton && (
+            <InnerButton command={command} {...buttonProps} ref={ref} />
+          )}
+        </ActionWrapper>
       </>
     )
   },
 )
 
 ActionButton.displayName = 'ActionButton'
+
+const InnerButton = forwardRef<
+  HTMLButtonElement,
+  Pick<ActionButtonProps, 'command' | 'children' | 'hideIcon'> &
+    ActionWrapperSlotProps
+>(({ isLoading, children, hideIcon = true, command, ...props }, ref) => {
+  const Icon = isLoading ? Loader2 : ArrowRight
+
+  return (
+    <Button type="button" {...props} ref={ref}>
+      <div className="flex flex-row items-center gap-2">
+        {children}
+        {command?.hideKeyShortcut ?? !command?.shortcut?.key ? null : (
+          <div className="flex flex-row items-center justify-center gap-1">
+            {command?.shortcut?.shift && <KeyShortcut>Shift</KeyShortcut>}
+            {command?.shortcut?.cmdCtrl && (
+              <KeyShortcut>{getCmdCtrlKey()}</KeyShortcut>
+            )}
+            <KeyShortcut>{command?.shortcut?.key.toUpperCase()}</KeyShortcut>
+          </div>
+        )}
+      </div>
+      {!hideIcon && (
+        <Icon className={cn('ml-2 h-4 w-4', isLoading && 'animate-spin')} />
+      )}
+    </Button>
+  )
+})
+
+InnerButton.displayName = 'InnerButton'
