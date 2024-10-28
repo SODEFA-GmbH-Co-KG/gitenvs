4970ba953098054a844badcdf4ce6021050bc3aa
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-28 06:54:03 +0100

    ✨ add project setup page

---
+++ src/app/setup/project/page.tsx
@@ -0,0 +1,40 @@
+import { redirect } from 'next/navigation'
+import { ActionButton } from '~/super-action/button/ActionButton'
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-center text-2xl">Setup your project</h1>
+
+      <p className="text-center">
+        Here are some last steps to configure your project.
+      </p>
+
+      <div className="grid grid-cols-[3fr_1fr] items-center gap-8">
+        <p>
+          To protect your passphrases add <code>*.gitenvs.passphrase</code> to
+          your <code>.gitignore</code> file.
+        </p>
+        <ActionButton
+          hideIcon={false}
+          action={async () => {
+            'use server'
+            console.log('clicked')
+          }}
+        >
+          Edit .gitignore
+        </ActionButton>
+      </div>
+
+      <ActionButton
+        action={async () => {
+          'use server'
+          redirect('/')
+        }}
+        className="self-end"
+      >
+        Done
+      </ActionButton>
+    </div>
+  )
+}
