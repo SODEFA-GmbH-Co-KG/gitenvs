b20285e918994101a6526bf8cbe0d929bff027e1
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-30 13:42:50 +0100

    🥅

--- @/components/HandlePastePassphrase.tsx
+++ @/components/HandlePastePassphrase.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Passphrase } from '@/gitenvs/gitenvs.schema'
+import { usePasteHandler } from '@/hooks/usePasteHandler'
 import { useSetAtom } from 'jotai'
 import { map } from 'lodash-es'
 import { useCallback } from 'react'
@@ -9,32 +10,37 @@ import {
   type StageEncryptionState,
   stageEncryptionStateAtom,
 } from './AtomifyPassphrase'
-import { usePasteHandler } from '@/hooks/usePasteHandler'
 
 export const HandlePastePassphrase = () => {
   const setStageEncryptionState = useSetAtom(stageEncryptionStateAtom)
 
   const handlePastedText = useCallback(
     (text: string) => {
-      const pastedPassphrases = z.array(Passphrase).safeParse(JSON.parse(text))
-      if (!pastedPassphrases.success) return
+      try {
+        const pastedPassphrases = z
+          .array(Passphrase)
+          .safeParse(JSON.parse(text))
+        if (!pastedPassphrases.success) return
 
-      setStageEncryptionState((prev) => {
-        if (!prev) return prev
+        setStageEncryptionState((prev) => {
+          if (!prev) return prev
 
-        const newState = map(prev, (passphrase) => {
-          if (passphrase.passphrase) return passphrase
-          const newPassphrase = pastedPassphrases.data.find(
-            (pp) => pp.stageName === passphrase.stageName,
-          )
-          return {
-            showValues: false,
-            passphrase: newPassphrase?.passphrase ?? null,
-            stageName: passphrase.stageName,
-          } satisfies StageEncryptionState
+          const newState = map(prev, (passphrase) => {
+            if (passphrase.passphrase) return passphrase
+            const newPassphrase = pastedPassphrases.data.find(
+              (pp) => pp.stageName === passphrase.stageName,
+            )
+            return {
+              showValues: false,
+              passphrase: newPassphrase?.passphrase ?? null,
+              stageName: passphrase.stageName,
+            } satisfies StageEncryptionState
+          })
+          return newState
         })
-        return newState
-      })
+      } catch (error) {
+        // ignore error
+      }
     },
     [setStageEncryptionState],
   )
