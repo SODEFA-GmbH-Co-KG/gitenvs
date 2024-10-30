4f5067b4dddc1c922fb6498edf77ce37aafa41ab
Sean Nicholas Dieterle <dieterle@sodefa.de>
2024-10-30 12:30:35 +0100

    Merge branch 'main' of github.com:SODEFA-GmbH-Co-KG/gitenvs into feature/HandlePastePassphrase

--- @/components/AtomifyPassphrase.tsx
+++ @/components/AtomifyPassphrase.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { Passphrase } from '@/gitenvs/gitenvs.schema'
 import { atom, useAtom } from 'jotai'
 import { map } from 'lodash-es'
-import { useCallback, useEffect } from 'react'
-import { z } from 'zod'
+import { useEffect } from 'react'
 import { useEncryptionKeyOnClient } from '~/utils/encryptionKeyOnClient'
 import {
   decryptWithEncryptionKey,
@@ -69,54 +67,6 @@ export const AtomifyPassphrase = ({
     setStageEncryptionState,
     stageEncryptionState,
   ])
-
-  const handlePaste = useCallback(
-    async (event: ClipboardEvent) => {
-      if (
-        event.target instanceof HTMLElement &&
-        event.target.tagName === 'INPUT'
-      )
-        return
-
-      const text = event.clipboardData?.getData('text')
-
-      if (!text) return
-
-      const pastedPassphrases = z.array(Passphrase).safeParse(JSON.parse(text))
-      console.log({ pastedPassphrases })
-
-      if (!pastedPassphrases.success) return
-
-      setStageEncryptionState((prev) => {
-        if (!prev) return prev
-
-        const newState = map(prev, (passphrase) => {
-          if (passphrase.decryptionKey) return passphrase
-          const newPassphrase = pastedPassphrases.data.find(
-            (pp) => pp.stageName === passphrase.stageName,
-          )
-          return {
-            showValues: false,
-            decryptionKey: newPassphrase?.passphrase ?? null,
-            stageName: passphrase.stageName,
-          } satisfies StageEncryptionState
-        })
-        return newState
-      })
-    },
-    [setStageEncryptionState],
-  )
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('paste', handlePaste)
-    }
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('paste', handlePaste)
-      }
-    }
-  }, [handlePaste])
 
   return null
 }
