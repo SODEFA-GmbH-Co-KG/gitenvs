fc0ec1d06beb5005e45ade309bf1c62426d4f0fb
Sean Nicholas Dieterle <dieterle@sodefa.de>
2025-03-08 16:37:12 +0100

    ♻️ client side

--- @/components/EditEnvKeyDialog.tsx
+++ @/components/EditEnvKeyDialog.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import {
   DialogDescription,
@@ -8,12 +10,9 @@ import {
 import { Input } from '@/components/ui/input'
 import { type EnvVar, type Gitenvs } from '@/gitenvs/gitenvs.schema'
 import { map } from 'lodash-es'
-import { ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import { saveGitenvs } from '~/lib/gitenvs'
-import {
-  streamDialog,
-  superAction,
-} from '~/super-action/action/createSuperAction'
+import { useShowDialog } from '~/super-action/dialog/DialogProvider'
 import { ActionForm } from '~/super-action/form/ActionForm'
 
 export const EditEnvKeyDialog = ({
@@ -25,31 +24,30 @@ export const EditEnvKeyDialog = ({
   gitenvs: Gitenvs
   dropdown: ReactNode
 }) => {
+  const showDialog = useShowDialog()
+
   return (
     <ActionForm
       action={async (formData) => {
-        'use server'
+        // This code runs on the client side intentionally
+        const key = formData.get('key')?.toString()
+        if (typeof key !== 'string') return
 
-        return superAction(async () => {
-          const key = formData.get('key')?.toString()
-          if (typeof key !== 'string') return
+        const newEnVars = map(gitenvs.envVars, (v) => {
+          if (v.id !== envVar.id) return v
 
-          const newEnVars = map(gitenvs.envVars, (v) => {
-            if (v.id !== envVar.id) return v
-
-            return {
-              ...v,
-              key,
-            }
-          })
-
-          await saveGitenvs({
-            ...gitenvs,
-            envVars: newEnVars,
-          })
-
-          streamDialog(null)
+          return {
+            ...v,
+            key,
+          }
         })
+
+        await saveGitenvs({
+          ...gitenvs,
+          envVars: newEnVars,
+        })
+
+        await showDialog(null)
       }}
     >
       <DialogHeader>
